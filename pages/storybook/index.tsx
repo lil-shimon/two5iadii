375610ce5889689568fa2d0367dd5f068793b7d5
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-04-15 00:06:59 +0900

    feat: 🎸 toggle example

---
+++ pages/storybook/index.tsx
@@ -0,0 +1,12 @@
+import { ToggleExample } from "../../components/atoms/toogle";
+
+const StoryBook = () => {
+  return (
+    <>
+      <h1>Toggle Example: </h1>
+      <ToggleExample />
+    </>
+  );
+};
+
+export default StoryBook;
